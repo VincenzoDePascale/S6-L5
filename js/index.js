3ec0531e148141bc371da30b70dd00c5eaed7117
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2023-01-20 16:57:04 +0100

    fix content

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function navAlert() {
+  alert("cercatele da solo le cose. Sono in ferie!");
+}
+
+function spongebobAlert() {
+  alert("Ho viste cose che voi umani non potreste immaginarvi");
+}
+
+function ciroAlert() {
+  alert("Bestemmi parecchio anche tu, non è vero?");
+}
+
+function dipreAlert() {
+  alert("Non essere triste: la vita è bella se non sei...");
+}
+
+function frankAlert() {
+  alert("Io nella vita non voglio fare un c***o");
+}
